94d50327c48fe7cd467a715f9c6d57ca238fedb5
carlosclaiton <carlosclaitonkuhn@gmail.com>
2023-12-08 05:20:10 +0000

    fixed the database connection string

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,13 +4,13 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 # put herr the connection string to your database
-# DATABASE_URL = "postgresql://myuser:test123@localhost:5432/webapp_db"
+DATABASE_URL_ind = "postgresql://myuser:test123@localhost:5432/webapp_db"
 
 # using here the name of the service from the dockercompose file for the service 
 # DATABASE_URL = "postgresql://myuser:test123@db:5432/webapp_db"
 
 # Getting the connection string from the enviroment passed though docker file
-DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASE_URL = os.environ.get("DATABASE_URL", DATABASE_URL_ind)
 
 # define the engine using the url
 engine = create_engine(DATABASE_URL)
